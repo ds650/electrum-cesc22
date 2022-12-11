6d7935bc3ac39042dab369feff6dccccd63a43a5
ds650 <39422200+ds650@users.noreply.github.com>
2022-12-11 22:24:41 +0000

    Update estimate_fee.py

--- electrum_cesc/scripts/estimate_fee.py
+++ electrum_cesc/scripts/estimate_fee.py
@@ -4,9 +4,9 @@ import asyncio
 from statistics import median
 from numbers import Number
 
-from electrum_ltc.network import filter_protocol, Network
-from electrum_ltc.util import create_and_start_event_loop, log_exceptions
-from electrum_ltc.simple_config import SimpleConfig
+from electrum_cesc.network import filter_protocol, Network
+from electrum_cesc.util import create_and_start_event_loop, log_exceptions
+from electrum_cesc.simple_config import SimpleConfig
 
 
 config = SimpleConfig()
