c6d5d1dc4a959ccb024316fcec8a345faa3308ca
ds650 <39422200+ds650@users.noreply.github.com>
2022-12-11 21:59:18 +0000

    Update qrwindow.py

--- electrum_cesc/gui/qt/qrwindow.py
+++ electrum_cesc/gui/qt/qrwindow.py
@@ -28,7 +28,7 @@ from PyQt5.QtWidgets import QHBoxLayout, QWidget
 
 from .qrcodewidget import QRCodeWidget
 
-from electrum_ltc.i18n import _
+from electrum_cesc.i18n import _
 
 
 class QR_Window(QWidget):
@@ -36,7 +36,7 @@ class QR_Window(QWidget):
     def __init__(self, win):
         QWidget.__init__(self)
         self.win = win
-        self.setWindowTitle('Electrum-LTC - '+_('Payment Request'))
+        self.setWindowTitle('Electrum-CESC - '+_('Payment Request'))
         self.setMinimumSize(800, 800)
         self.setFocusPolicy(Qt.NoFocus)
         main_box = QHBoxLayout()
